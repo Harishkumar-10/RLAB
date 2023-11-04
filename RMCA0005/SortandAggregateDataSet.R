a9bd5c7792a570ea3676529bd260fa96f05dd829
Harishkumar-10 <98449782+Harishkumar-10@users.noreply.github.com>
2023-11-04 11:21:00 +0530

    Add files via upload

---
+++ RMCA0005/SortandAggregateDataSet.R
@@ -0,0 +1,13 @@
+vector_data <- c(101,102,103,104,105)
+
+sorted_vector <- sort(vector_data)
+
+# Merging two vectors
+vector1 <- c(1, 2, 3)
+vector2 <- c(4, 5, 6)
+merged_vector <- c(vector1, vector2)
+
+# Aggregating data with mean and sum
+data <- c(12, 15, 18, 24, 9)
+mean_data <- mean(data)
+sum_data <- sum(data)
---
+++ RMCA0005/VectorandMatrix.R
@@ -0,0 +1,13 @@
+# Creating a vector and a matrix
+vector_data <- c(101,102,103,104,105)
+matrix_data <- matrix(1:9, nrow = 3)
+
+# Accessing elements in a vector and matrix
+vector_element <- vector_data[3]
+matrix_element <- matrix_data[2, 2]
+
+# Performing operations on vectors and matrices
+vector_sum <- sum(vector_data)
+matrix_transpose <- t(matrix_data)
+
+
